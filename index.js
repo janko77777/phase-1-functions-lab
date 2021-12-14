85a0c898079b290e1fb3cf6855bbd05bc37ef30e
Steven Jankowski <steven.jankowski1@gmail.com>
2021-12-14 13:48:49 +0000

    commit tree 5be9229db64e99f137cdbf7d7bf5d356ea5dacb8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+let pickUpLocation = 46
+
+function distanceFromHqInBlocks (pickUpLocation) { 
+    return Math.abs(42 - pickUpLocation)
+}
+
+function distanceFromHqInFeet (pickUpLocation) {
+    return distanceFromHqInBlocks (pickUpLocation) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice (start, destination) {
+    let distance = distanceTravelledInFeet (start, destination)
+    let fare
+    if (distance <= 400) {
+      fare = 0
+      return fare
+    } else if (distance > 400 && distance <= 2000) {
+      fare = (distance - 400) * 0.02
+      return fare
+    } else if (distance > 2000 && distance <=2500) {
+      fare = 25
+      return fare
+    } else return "cannot travel that far"
+}
